04947049eb6ebddd6f58bc4217427766ad7b1fdd
Luis Vasquez <levasquez00@gmail.com>
2020-03-08 23:32:11 -0500

    remove console date

--- dotnetcore/myApp/Program.cs
+++ dotnetcore/myApp/Program.cs
@@ -7,7 +7,8 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
+          
+          
         }
     }
 }
